719bb4a9b2513b83b08b01ee243f7e06038e7f81
agungfir98 <agungfir98@gmail.com>
2024-08-20 15:22:58 +0700

    feat: calendar service

---
+++ api/calendar.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"context"
+	"gcal-cli/auth"
+	"log"
+	"os"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+)
+
+func GetCalendar() *calendar.Service {
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("unable to read credentials file: %v\n", err)
+	}
+
+	cfg, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope)
+	if err != nil {
+		log.Fatalf("unable to parse client secret file to config: %v\n", err)
+	}
+
+	client := auth.GetClient(cfg)
+
+	srv, err := calendar.NewService(context.Background(), option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("unable to retreive calendar client: %v\n", err)
+	}
+	return srv
+}

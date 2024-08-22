9c081138e665cdb8a2a4716a49efb6d5e702cbb9
agungfir98 <agungfir98@gmail.com>
2024-08-22 17:18:31 +0700

    chore: add end of day util function

---
+++ utils/time_utils/time_utils.go
@@ -0,0 +1,11 @@
+package timeutils
+
+import "time"
+
+var DefaultLayout = "02 01 2006 15:04"
+
+func EndOfDay(t time.Time) time.Time {
+	y, m, d := t.Date()
+
+	return time.Date(y, m, d, 23, 59, 59, 0, t.Location())
+}

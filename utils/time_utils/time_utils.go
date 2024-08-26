a7eea226ff3d1cd253309c3271b8a3d21cd3c3d9
agungfir98 <agungfir98@gmail.com>
2024-08-26 14:57:37 +0700

    refactor: remove time parameter

--- utils/time_utils/time_utils.go
+++ utils/time_utils/time_utils.go
@@ -4,7 +4,8 @@ import "time"
 
 var DefaultLayout = "02 01 2006 15:04"
 
-func EndOfDay(t time.Time) time.Time {
+func EndOfDay() time.Time {
+	t := time.Now()
 	y, m, d := t.Date()
 
 	return time.Date(y, m, d, 23, 59, 59, 0, t.Location())

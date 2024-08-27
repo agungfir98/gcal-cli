8cdec01bad643a8460ae03d3d8e6502475d90f8a
agungfir98 <agungfir98@gmail.com>
2024-08-27 16:23:51 +0700

    refactor: put back the endOfDay time param

--- utils/time_utils/time_utils.go
+++ utils/time_utils/time_utils.go
@@ -5,8 +5,7 @@ import "time"
 var DefaultLayout = "02 01 2006 15:04"
 var AllDayDefaultLayout = "2006-01-02"
 
-func EndOfDay() time.Time {
-	t := time.Now()
+func EndOfDay(t time.Time) time.Time {
 	y, m, d := t.Date()
 
 	return time.Date(y, m, d, 23, 59, 59, 0, t.Location())

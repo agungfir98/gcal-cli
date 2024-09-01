b4619e53d92bc0e80b72c510956f9a75bcc1cab7
agungfir98 <agungfir98@gmail.com>
2024-09-01 13:42:43 +0700

    refactor: making two default layout for date and date with time specific layout

--- utils/time_utils/time_utils.go
+++ utils/time_utils/time_utils.go
@@ -2,7 +2,8 @@ package timeutils
 
 import "time"
 
-var DefaultLayout = "02 01 2006 15:04"
+var DefaultLayout = "02 01 2006"
+var DefaultLayoutWithTime = "02 01 2006 15:04"
 var AllDayDefaultLayout = "2006-01-02"
 
 func EndOfDay(t time.Time) time.Time {

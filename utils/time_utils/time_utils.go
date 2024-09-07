ff7aaa637150145d18b772c304d69e6a65444c2e
agungfir98 <agungfir98@gmail.com>
2024-09-07 21:41:46 +0700

    chore: use ISO8601 date format

--- utils/time_utils/time_utils.go
+++ utils/time_utils/time_utils.go
@@ -1,6 +1,10 @@
 package timeutils
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 var DefaultLayout = "02 01 2006"
 var DefaultLayoutWithTime = "02 01 2006 15:04"
@@ -10,4 +14,58 @@ func EndOfDay(t time.Time) time.Time {
 	y, m, d := t.Date()
 
 	return time.Date(y, m, d, 23, 59, 59, 0, t.Location())
+}
+
+func ParseTimeLayout(input string) (time.Time, error) {
+	now := time.Now()
+	var err error
+
+	for _, layout := range layouts {
+		t, err := time.ParseInLocation(layout, input, now.Location())
+		if err == nil {
+			return t, nil
+		}
+	}
+	return time.Time{}, fmt.Errorf("unable to parse date: %v\n", err)
+}
+
+var layouts = []string{
+	"02 01 2006 15:04",
+	"02-01-2006 15:04",
+	"02-01-2006 3:04 PM",
+	"2006-01-02 15:04",
+	"02/01/2006 15:04",
+	"2006-01-02",
+	"02/01/2006",
+	"15:04",
+	"3:04 PM",
+}
+
+var formatMap = map[string]string{
+	"%a": "Mon",
+	"%A": "Monday",
+	"%b": "Jan",
+	"%B": "January",
+	"%d": "02",
+	"%m": "01",
+	"%Y": "2006",
+	"%y": "06",
+	"%H": "15",
+	"%I": "03",
+	"%p": "PM",
+	"%M": "04",
+	"%S": "05",
+}
+
+func ConvertToGoLayout(format string) string {
+	for cFormat, goLayout := range formatMap {
+		format = strings.Replace(format, cFormat, goLayout, -1)
+	}
+	return format
+}
+
+func ParseWithCustomFormat(format, input string) (time.Time, error) {
+	t := time.Now()
+	goLayout := ConvertToGoLayout(format)
+	return time.ParseInLocation(goLayout, input, t.Location())
 }
